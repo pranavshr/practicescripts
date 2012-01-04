34b67c76b3a99575b65a53cd700fed6b5fda6db0
Pranav Shrestha <pranav@uvic.ca>
2012-01-04 11:22:24 -0800

    first commit with try.py

---
+++ try.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+
+i = input ("Enter a number:")
+elif(i<0):
+	print("it is a negative number")
+elif (i>0):
+	print("it is a positive number")
+elif(i==0):
+	print("number is zero")
+
